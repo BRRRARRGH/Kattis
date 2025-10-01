d3cbc15c78dc6986391137d4fb8aeb1b5ef91788
BRRRARRGH <thure.w.johnson@gmail.com>
2025-10-01 09:55:39 -0500

    total kattis repo

---
+++ ProgrammingTeam/Haiku/haiku.py
@@ -0,0 +1,26 @@
+def syllableCheck(phrase, totalSum):
+    for s in myList:
+        if phrase.StartsWith(s):
+
+    if phrase in myList:
+        return totalSum
+    
+
+
+
+numLines = int(input())
+myList = []
+numS1 = 0
+numS2 = 0
+numS3 = 0
+for x in range(numLines):
+    myList.append(input())
+
+
+
+if(numS1 == 5 and numS2 == 7 and numS3 == 5):
+    print("haiku")
+else:
+    print("come back next year")
+
+
