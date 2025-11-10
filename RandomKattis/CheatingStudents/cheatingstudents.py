ad94cec76c8679d995554e2fc34f4d4af7a6ce3e
BRRRARRGH <thure.w.johnson@gmail.com>
2025-11-09 19:13:10 -0600

    kattis shit

--- RandomKattis/CheatingStudents/cheatingstudents.py
+++ RandomKattis/CheatingStudents/cheatingstudents.py
@@ -1,16 +1,24 @@
 numTime = 0
-def checkTimes(has, no):
-    global numTime
-    if(len(doneLocations) == 0):
-        return 0
-    else:
-        return 1
+potLocations = []
+doneLocations = []
+minLength = 20000
+shortestIndex = 0
+distance = 0
 
 numStudents = int(input())
-potlocations = []
-doneLocations = []
 for x in range(numStudents):
     student = input().split()
-    potlocations.append([int(y) for y in student])
-print(checkTimes(potLocations, doneLocations))
-print(potlocations)
+    potLocations.append([int(y) for y in student])
+doneLocations.append(potLocations.pop(0))
+while(len(potLocations) != 0):
+    minLength = 20000
+    shortestIndex = 0
+    for i in range(len(doneLocations)):
+        for j in range(len(potLocations)):
+            distance = abs(doneLocations[i][0] - potLocations[j][0]) + abs(doneLocations[i][1] - potLocations[j][1])
+            if distance < minLength:
+                minLength = distance
+                shortestIndex = j
+    doneLocations.append(potLocations.pop(shortestIndex))
+    numTime += minLength * 2
+print(numTime)
